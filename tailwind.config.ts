d6997be7c23c7430f8458ee282ca87634efc140d
Billy Castro <billycastro.1822@gamil.com>
2024-11-26 12:48:06 -0500

    configuracion inicial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,15 @@ export default {
   theme: {
     extend: {
       colors: {
+        secondary: "#f5741c",
+        darkBg: '#131424',
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage:{
+        "gradient-Cover":
+        "linear-gradient(90.21deg, rgba(70,54, 124, 0.5) -91%, rgba(74, 47,189, 0.5) 111,58%)",
+      }
     },
   },
   plugins: [],
